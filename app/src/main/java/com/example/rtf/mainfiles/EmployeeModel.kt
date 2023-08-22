8d3485d89d81edd2d686fc0cfb544feb84400773
hp <sajaykumar323@rku.ac.in>
2023-08-22 09:31:13 +0530

    -initial commit

---
+++ app/src/main/java/com/example/rtf/mainfiles/EmployeeModel.kt
@@ -0,0 +1,9 @@
+package com.example.rtf.mainfiles
+
+
+data class EmployeeModel(
+    var empId: String = "",
+    var empName: String = "",
+    var empAge: String = "",
+    var empSalary: String = ""
+)
